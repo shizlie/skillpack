5d1903b63f7d8bab78a0c46f488b01cff425ac11
Bao Harry Ngo <ngoduybao@gmail.com>
2026-06-01 23:57:56 +0700

    feat(core): re-export routes and matchRoute from package index

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,6 +1,7 @@
 export { createInMemoryLeaseStore } from "./storage.js";
 export { createD1LeaseStore, ensureD1Schema } from "./storage-d1.js";
 export { createLicenseFetchHandler, startLicenseServer } from "./server.js";
+export { routes, matchRoute } from "./routes.js";
 export { draftInvoiceFromUsage, findPricingRuleForUsage } from "./billing.js";
 export {
   createDodoPaymentProvider,
